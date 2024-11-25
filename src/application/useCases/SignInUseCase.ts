5ff4b85fe9bebbdddb71c090fde98bf349e0cb9e
Ca788 <carlos0ficial500@gmail.com>
2024-11-25 01:39:40 -0300

    first commit

---
+++ src/application/useCases/SignInUseCase.ts
@@ -0,0 +1,44 @@
+import { prismaClient } from "../lib/prismaClient";
+import { InvalidCredentials } from "../erros/InvalidCredentials";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { env } from "../config/env";
+
+
+interface IInput {
+  email: string;
+  password: string;
+}
+
+interface IOutput {
+  accessToken: string;
+}
+
+export class SignUpUseCase {
+ async call({ email, password }: IInput): Promise<IOutput> {
+    const account = await prismaClient.account.findUnique({
+      where: { email },
+    });
+
+    if (!account) {
+      throw new InvalidCredentials();
+    }
+
+    const isPasswordValid = await compare(password, account.password);
+
+    if(!isPasswordValid) {
+      throw new InvalidCredentials();
+    }
+
+    const accessToken = sign(
+      { subj: account.id },
+      env.jwtSecret,
+      { expiresIn: '1d' },
+    );
+
+    return {
+      accessToken
+    };
+  }
+}
+
